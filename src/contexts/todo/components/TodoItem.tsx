fd14f03bc0b81fb41becc625919a9d656427a0bb
[dyad] <honoriodudu@gmail.com>
2026-06-18 17:57:04 -0300

    [dyad] Corrigindo importação faltando do useState - wrote 1 file(s)

--- src/contexts/todo/components/TodoItem.tsx
+++ src/contexts/todo/components/TodoItem.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Badge } from "@/components/ui/badge";
