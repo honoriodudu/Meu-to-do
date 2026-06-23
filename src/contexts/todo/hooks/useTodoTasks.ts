3d72cf7154c01f877b9432062e5b0e43cabc15d5
[dyad] <honoriodudu@gmail.com>
2026-06-22 23:48:52 -0300

    Updated src/contexts/todo/hooks/useTodoTasks.ts

--- src/contexts/todo/hooks/useTodoTasks.ts
+++ src/contexts/todo/hooks/useTodoTasks.ts
@@ -1,6 +1,6 @@
-// ARQUIVO: src/contexts/todo/hooks/useTodoTasks.ts
 import { useQuery } from "@tanstack/react-query";
 import { fetchUserTodos, todoQueryKeys } from "../services/todo.service";
+import type { TodoTask } from "../todo.types";
 
 export function useTodoTasks(userId: string | undefined) {
   return useQuery({
